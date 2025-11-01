2109986f1c44d87b56f39571e866fe024850bda5
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-01 07:53:02 +0200

    finish Part 07 Product Module - Entities Configurations and DbContext

---
+++ E Commerce.Persistence/Data/Configurations/ProductConfiguration.cs
@@ -0,0 +1,37 @@
+﻿using E_Commerce.Domain.Entities.ProductModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Data.Configurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.Property(x => x.Name)
+    .HasMaxLength(100);
+
+            builder.Property(x => x.Description)
+                .HasMaxLength(500);
+
+            builder.Property(x => x.PictureUrl)
+                .HasMaxLength(200);
+
+            builder.Property(x => x.Price)
+                .HasPrecision(18, 2);
+
+            builder.HasOne(x => x.ProductBrand)
+    .WithMany()
+    .HasForeignKey(x => x.BrandId);
+
+            builder.HasOne(x => x.ProductType)
+                .WithMany()
+                .HasForeignKey(x => x.TypeId);
+        }
+    }
+}
--- E Commerce.Web/Program.cs
+++ E Commerce.Web/Program.cs
@@ -1,4 +1,7 @@
 
+using E_Commerce.Persistence.Data.DbContexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace E_Commerce.Web
 {
     public class Program
@@ -13,7 +16,12 @@ namespace E_Commerce.Web
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen(); 
+            builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
             #endregion
 
             var app = builder.Build();
