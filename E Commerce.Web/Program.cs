079585a15158b4a71c50a1b42b0b615ce868ef78
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-01 05:54:12 +0200

    finish Part 01 Project Architecture

--- E Commerce.Web/Program.cs
+++ E Commerce.Web/Program.cs
@@ -4,7 +4,7 @@ namespace E_Commerce.Web
     public class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
