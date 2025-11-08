40c1d1cb67c6299b52e8c8e4d8406248c7f00f13
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-08 20:03:32 +0200

    finish video04 generic repo and unit of work

---
+++ E Commerce.Domain/Contract/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using E_Commerce.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Contract
+{
+    public  interface IGenericRepository<TEntity,TKey> where TEntity : BaseEntity<TKey>
+    {
+        Task<IEnumerable<TEntity>> GetAllAsync();
+        Task<TEntity?> GetByIdAsync(TKey id);
+        Task AddAsync(TEntity entity);
+        void Update(TEntity entity);
+        void Remove(TEntity entity);
+    }
+}
---
+++ E Commerce.Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿using E_Commerce.Domain.Contract;
+using E_Commerce.Domain.Entities;
+using E_Commerce.Persistence.Data.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly Dictionary<Type, object> _repositories = [];
+        private readonly StoreDbContext _dbContext;
+
+        public UnitOfWork(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IGenericRepository<TEntity, TKey> GetRepository<TEntity, TKey>() where TEntity : BaseEntity<TKey>
+        {
+           var EntityType = typeof(TEntity);
+            if (_repositories.TryGetValue(EntityType,out object? repository))
+            return (IGenericRepository<TEntity, TKey>)repository;
+           var NewRepo = new GenericRepository<TEntity, TKey>(_dbContext);
+                _repositories[EntityType] = NewRepo;
+                return NewRepo;
+            
+        }
+
+        public Task<int> SaveChangesAsync() => _dbContext.SaveChangesAsync();
+    }
+}
