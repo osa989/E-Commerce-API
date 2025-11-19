6b345e07dccb24b68e73bdd4dc154e9b68503314
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-19 04:23:23 +0200

    finish part 5 basket module basket repository

---
+++ E Commerce.Persistence/Repositories/BasketRepository.cs
@@ -0,0 +1,56 @@
+﻿using E_Commerce.Domain.Contract;
+using E_Commerce.Domain.Entities.BasketModule;
+using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Repositories
+{
+    public class BasketRepository : IBasketRepository
+    {
+        private readonly IDatabase _database;
+
+        public BasketRepository(IConnectionMultiplexer connection)
+        {
+            _database = connection.GetDatabase();
+        }
+        public async Task<CustomerBasket?> CreateIrUpdateBasketAsync(CustomerBasket basket, TimeSpan timeToLive = default)
+        {
+            var JsonBasket = JsonSerializer.Serialize(basket);
+            var IsCreatedOrUpdated = await _database.StringSetAsync(basket.Id, JsonBasket,
+                 (timeToLive == default) ? TimeSpan.FromDays(7) : timeToLive);
+                
+            if (IsCreatedOrUpdated)
+            {
+                //var Basket = await _database.StringGetAsync(basket.Id);
+                //return JsonSerializer.Deserialize<CustomerBasket>(Basket!);
+                return await GetBasketAsync(basket.Id);
+
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteBasketAsync(string basketId)=>await _database.KeyDeleteAsync(basketId);
+
+        public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+            var Basket = await _database.StringGetAsync(basketId);
+            if(string.IsNullOrEmpty(Basket))
+            {
+                return null;
+            }
+            else
+            {
+                return JsonSerializer.Deserialize<CustomerBasket>(Basket!);
+            }
+        }
+    }
+}
