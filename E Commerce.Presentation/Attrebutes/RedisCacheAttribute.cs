d141642bd6b66b8e1e6f947e25eee623db6a9b36
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-22 08:38:27 +0200

    finish part 02 attributes - action filter attribute

---
+++ E Commerce.Presentation/Attrebutes/RedisCacheAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Presentation.Attrebutes
+{
+    internal class RedisCacheAttribute : ActionFilterAttribute 
+    {
+        public override Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            return base.OnActionExecutionAsync(context, next);
+        }
+    }
+}
+ 
