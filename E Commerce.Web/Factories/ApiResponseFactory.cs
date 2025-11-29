0b41d8d2de93d117953961da52c0e522fee62f37
OsamaGamal120 <osama12100519@cs.modern-academy.edu.eg>
2025-11-29 07:44:58 +0200

    finish validation errors

---
+++ E Commerce.Web/Factories/ApiResponseFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce.Web.Factories
+{
+    public static class ApiResponseFactory
+    {
+        public static IActionResult GenerateApiValidationResponse(ActionContext actionContext) {
+        
+            
+                #region explaining of model state errors
+                // Product 
+
+                //[Required] // Model state01
+                //[MaxLength = 20]  //Model state02 
+                // Name
+                //[Required]  // Model state03
+                // Id 
+                //[Required] // Model state04
+                //[Range (1,1000)] // Model state05
+                // Price
+                //[Range (1,100)] // Model state06
+                // Quantity
+
+                // key : Model property Name
+                // value : array of error messages 
+                #endregion
+
+                var errors = actionContext.ModelState.Where(X => X.Value.Errors.Count > 0)
+                .ToDictionary(X => X.Key,
+                X => X.Value.Errors.Select(X => X.ErrorMessage).ToArray());
+
+                var Problem = new ProblemDetails()
+                {
+                    Title = " Validation Error",
+                    Detail = "One or more vlalidation errors occurred",
+                    Status = StatusCodes.Status400BadRequest,
+                    Extensions =
+                        {
+                            { "Errors", errors }
+                        }
+                };
+                return new BadRequestObjectResult(Problem);
+            }
+        
+    }
+}
--- E Commerce.Web/Program.cs
+++ E Commerce.Web/Program.cs
@@ -8,6 +8,7 @@ using E_Commerce.Services.MappingProfiles;
 using E_Commerce.Services_Abstraction;
 using E_Commerce.Web.CustomMiddlewares;
 using E_Commerce.Web.Extentions;
+using E_Commerce.Web.Factories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackExchange.Redis;
@@ -52,42 +53,7 @@ namespace E_Commerce.Web
             builder.Services.AddScoped<ICacheService, CacheService>();
             builder.Services.Configure<ApiBehaviorOptions>(options =>
             {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    #region explaining of model state errors
-                    // Product 
-
-                    //[Required] // Model state01
-                    //[MaxLength = 20]  //Model state02 
-                    // Name
-                    //[Required]  // Model state03
-                    // Id 
-                    //[Required] // Model state04
-                    //[Range (1,1000)] // Model state05
-                    // Price
-                    //[Range (1,100)] // Model state06
-                    // Quantity
-
-                    // key : Model property Name
-                    // value : array of error messages 
-                    #endregion
-
-                    var errors = actionContext.ModelState.Where(X => X.Value.Errors.Count > 0)
-                    .ToDictionary(X => X.Key,
-                    X => X.Value.Errors.Select(X => X.ErrorMessage).ToArray());
-
-                    var Problem = new ProblemDetails()
-                    {
-                        Title = " Validation Error",
-                        Detail = "One or more vlalidation errors occurred",
-                        Status = StatusCodes.Status400BadRequest,
-                        Extensions =
-                        {
-                            { "Errors", errors }
-                        }
-                    };
-                    return new BadRequestObjectResult(Problem);
-                };
+                options.InvalidModelStateResponseFactory = ApiResponseFactory.GenerateApiValidationResponse;
             });
             #endregion
 
