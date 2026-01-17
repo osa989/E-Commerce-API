7be6a1d04f3b24f155a35ae835fd30b7504ba2f9
osa989 <osamagamal9988@gmail.com>
2026-01-17 17:09:07 +0200

    order entity and configuration with dataseed for deliverty method part 05

---
+++ E Commerce.Domain/Entities/OrderModule/Order.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Domain.Entities.OrderModule
+{
+    public class Order : BaseEntity<int>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public OrderAddress Address { get; set; } = default!;
+
+        public DeliveryMethod DeliveryMethod { get; set; } = default!;
+
+        public ICollection<OrderItem> Items { get; set; } = [];
+
+        public decimal Subtotal { get; set; }
+        //public decimal Total { get; set; }// can be calculated in runtime or database 
+        public decimal GetTotal() => Subtotal + DeliveryMethod.Price; //runtime calculation
+    }
+}
---
+++ E Commerce.Persistence/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using E_Commerce.Domain.Entities.OrderModule;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Persistence.Data.Configurations
+{
+    public class OrderConfiguration : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.Property(X=> X.Subtotal)
+                .HasPrecision(8,2);
+
+            builder.OwnsOne(X=> X.Address, OEntity =>
+            {
+                OEntity.Property(X => X.FirstName).HasMaxLength(50).IsRequired();
+                OEntity.Property(X => X.LastName).HasMaxLength(50).IsRequired();
+                OEntity.Property(X => X.Street).HasMaxLength(50).IsRequired();
+                OEntity.Property(X => X.City).HasMaxLength(50).IsRequired();
+            });
+        }
+    }
+}
