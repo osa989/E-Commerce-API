17fb22054107466d1919ac4ea2c8ebe4ae4e11a3
osa989 <osamagamal9988@gmail.com>
2026-01-18 14:49:56 +0200

    switch

--- E Commerce.Shared/DTOs/OrderDTO/OrderToReturnDTO.cs
+++ E Commerce.Shared/DTOs/OrderDTO/OrderToReturnDTO.cs
@@ -7,5 +7,5 @@ using System.Threading.Tasks;
 namespace E_Commerce.Shared.DTOs.OrderDTO
 {
     public record OrderToReturnDTO(Guid Id, string UserEmail,ICollection<OrderItemDTO> Items,AddressDTO Address,
-        string DeliveryMethod, string OrderStatus,DateTimeOffset OrderDate,decimal SubTotal, decimal Total)
+        string DeliveryMethod, string OrderStatus,DateTimeOffset OrderDate,decimal SubTotal, decimal Total);
 }
